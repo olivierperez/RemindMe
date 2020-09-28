6be9b8117b39119fd00d303af3e4de8c3a87d008
Olivier PEREZ <olivier@olivierperez.fr>
2020-09-28 20:49:16 +0200

    Display specific icons in notifications

--- app/src/main/java/fr/o80/remindme/presentation/MainActivity.kt
+++ app/src/main/java/fr/o80/remindme/presentation/MainActivity.kt
@@ -20,19 +20,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.showNotification).setOnClickListener {
-            val message = if (shouldGoToWork(Calendar.getInstance())) {
-                R.string.goToWork_message
+            if (shouldGoToWork(Calendar.getInstance())) {
+                popupNotification(
+                    notificationId = MORNING_REMINDER_ID,
+                    message = R.string.goToWork_message,
+                    smallIcon = R.drawable.ic_workplace,
+                    channelId = MORNING_REMINDER_CHANNELID,
+                    channelName = R.string.morningReminder_channel
+                )
             } else {
-                R.string.stayAtHome_message
+                popupNotification(
+                    notificationId = MORNING_REMINDER_ID,
+                    message = R.string.stayAtHome_message,
+                    smallIcon = R.drawable.ic_home,
+                    channelId = MORNING_REMINDER_CHANNELID,
+                    channelName = R.string.morningReminder_channel
+                )
             }
 
-            popupNotification(
-                notificationId = MORNING_REMINDER_ID,
-                message = message,
-                smallIcon = R.drawable.ic_launcher_foreground,
-                channelId = MORNING_REMINDER_CHANNELID,
-                channelName = R.string.morningReminder_channel
-            )
         }
 
     }
