7eac19c99796321d4ac693039a10ef5068a7d102
Olivier PEREZ <olivier@olivierperez.fr>
2020-09-29 20:10:39 +0200

    Improve readability of ViewModel tests

--- app/src/test/java/fr/o80/remindme/presentation/HomeViewModelTest.kt
+++ app/src/test/java/fr/o80/remindme/presentation/HomeViewModelTest.kt
@@ -15,10 +15,13 @@ import io.mockk.mockkStatic
 import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(MockKExtension::class)
+@DisplayName("Home screen ViewModel")
 internal class HomeViewModelTest {
 
     @MockK
@@ -38,55 +41,71 @@ internal class HomeViewModelTest {
         }
     }
 
-    @Test
-    fun shouldGoToWork() {
-        every { shouldGoToWorkUseCase.invoke(any()) } returns true
+    @Nested
+    @DisplayName("When it's a day at work place")
+    inner class GoToWork {
 
-        viewModel.onCreate()
+        @BeforeEach
+        fun stayAtHome() {
+            every { shouldGoToWorkUseCase.invoke(any()) } returns true
+        }
 
-        assertEquals(R.drawable.ic_workplace, viewModel.state.value?.icon)
-    }
+        @Test
+        @DisplayName("Show the right icon")
+        fun shouldGoToWork() {
+            viewModel.onCreate()
 
-    @Test
-    fun shouldNotifyGoToWork() {
-        every { shouldGoToWorkUseCase.invoke(any()) } returns true
+            assertEquals(R.drawable.ic_workplace, viewModel.state.value?.icon)
+        }
 
-        viewModel.onButtonClicked()
+        @Test
+        @DisplayName("Popup the notification")
+        fun shouldNotifyGoToWork() {
+            viewModel.onButtonClicked()
 
-        verify {
-            popupNotificationUseCase.invoke(
-                notificationId = MORNING_REMINDER_ID,
-                message = R.string.goToWork_message,
-                smallIcon = R.drawable.ic_workplace,
-                channelId = MORNING_REMINDER_CHANNELID,
-                channelName = R.string.morningReminder_channel
-            )
+            verify {
+                popupNotificationUseCase.invoke(
+                    notificationId = MORNING_REMINDER_ID,
+                    message = R.string.goToWork_message,
+                    smallIcon = R.drawable.ic_workplace,
+                    channelId = MORNING_REMINDER_CHANNELID,
+                    channelName = R.string.morningReminder_channel
+                )
+            }
         }
     }
 
-    @Test
-    fun shouldStayAtHome() {
-        every { shouldGoToWorkUseCase.invoke(any()) } returns false
+    @Nested
+    @DisplayName("When it's a couch day")
+    inner class StayAtHome {
 
-        viewModel.onCreate()
+        @BeforeEach
+        fun stayAtHome() {
+            every { shouldGoToWorkUseCase.invoke(any()) } returns false
+        }
 
-        assertEquals(R.drawable.ic_home, viewModel.state.value?.icon)
-    }
+        @Test
+        @DisplayName("Show the right icon")
+        fun shouldStayAtHome() {
+            viewModel.onCreate()
 
-    @Test
-    fun shouldNotifyStayAtHome() {
-        every { shouldGoToWorkUseCase.invoke(any()) } returns false
+            assertEquals(R.drawable.ic_home, viewModel.state.value?.icon)
+        }
 
-        viewModel.onButtonClicked()
+        @Test
+        @DisplayName("Popup the notification")
+        fun shouldNotifyStayAtHome() {
+            viewModel.onButtonClicked()
 
-        verify {
-            popupNotificationUseCase.invoke(
-                notificationId = MORNING_REMINDER_ID,
-                message = R.string.stayAtHome_message,
-                smallIcon = R.drawable.ic_home,
-                channelId = MORNING_REMINDER_CHANNELID,
-                channelName = R.string.morningReminder_channel
-            )
+            verify {
+                popupNotificationUseCase.invoke(
+                    notificationId = MORNING_REMINDER_ID,
+                    message = R.string.stayAtHome_message,
+                    smallIcon = R.drawable.ic_home,
+                    channelId = MORNING_REMINDER_CHANNELID,
+                    channelName = R.string.morningReminder_channel
+                )
+            }
         }
     }
 }
